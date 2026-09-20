43a5741ef97570a5a934c7591bb69db69e17ecf7
miguelavieyrag-ship-it <miguel.a.vieyra.g@gmail.com>
2026-09-20 11:01:49 -0600

    Add files via upload

---
+++ NobelOK.py
@@ -0,0 +1,55 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
+st.write(''' # Describe the research you conducted to win the Nobel Prize ''')
+st.image("07Scott-GREATNESS-articleLarge.webp", caption="Alfred Nobel invented dynamite, setting a challenge for the whole world—could you win the Nobel Prize?.")
+
+st.header('Texto')
+def user_input_features():
+  # Entrada
+  texto = st.text_input("Write a statement explaining why you deserve to be awarded the Nobel Prize.")
+
+  user_input_data = {'Text': texto}
+
+  features = pd.DataFrame(user_input_data, index=[0])
+
+  return features
+
+df = user_input_features()
+
+nobel =  pd.read_csv('df_nobel_ok.csv', encoding='latin-1')
+X = nobel.Claen_Mot
+y = nobel.Lable_num
+
+vect = CountVectorizer()
+X_dtm = vect.fit_transform(X)
+
+nb = MultinomialNB()
+nb.fit(X_dtm, y)
+
+#nobeldf['Lable_num']=nobeldf.Category.map({"chemistry":0,"economics":1,"literature":2,"medicine":3,"peace":4,"physics":5})
+
+if df['Text'].iloc[] == "":
+  st.write('No forecast')
+else:
+      df_dtm = vect.transform(df['Text'])
+      prediction = nb.predict(df_dtm)
+      st.subheader('The prediction of the prize based on your motivation is:')
+      if prediction == 0:
+        st.write('Chemistry')
+      elif prediction == 1:
+        st.write('Economics')
+      elif prediction == 2:
+        st.write('Literature')
+      elif prediction == 3:
+        st.write('Medicine')
+      elif prediction == 4:
+        st.write('Peace')
+      elif prediction == 5:
+        st.write('Physics')
+      else:
+        st.write('No forecast')
